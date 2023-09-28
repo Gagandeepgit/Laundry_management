dfd5890c13901cffcc05faa0d020fe341d0863a7
Gagandeep Patbandha <gagandeep.patbandha@hyscaler.com>
2023-09-29 01:31:50 +0530

    Dockerfile added

--- loginsystem/includes/config.php
+++ loginsystem/includes/config.php
@@ -1,15 +1,13 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_SERVER', 'db');
+define('DB_USER', 'root');
+define('DB_PASS', 'root_password');
 define('DB_NAME', 'laundry_db');
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
 // Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
- }
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
 ?>
-
